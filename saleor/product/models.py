56d5f66e3c06c75b019c05c19e0c3ec9f02b4920
Mateusz Dereniowski <derenio@gmail.com>
2019-07-05 15:09:04 +0200

    Add "minimal_variant_price" to the Product model

--- saleor/product/models.py
+++ saleor/product/models.py
@@ -133,6 +133,11 @@ class Product(SeoModel, PublishableModel):
         max_digits=settings.DEFAULT_MAX_DIGITS,
         decimal_places=settings.DEFAULT_DECIMAL_PLACES,
     )
+    minimal_variant_price = MoneyField(
+        currency=settings.DEFAULT_CURRENCY,
+        max_digits=settings.DEFAULT_MAX_DIGITS,
+        decimal_places=settings.DEFAULT_DECIMAL_PLACES,
+    )
     attributes = HStoreField(default=dict, blank=True)
     updated_at = models.DateTimeField(auto_now=True, null=True)
     charge_taxes = models.BooleanField(default=True)
